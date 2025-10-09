0d1b34d2f86e4959ff9c74f6204ec55a1a400f71
Jaspreet Singh <work.jaspreet1203@gmail.com>
2025-10-10 01:55:35 +0530

    MODIFYING - increased security by adding a layer between the backend server and the database using SQLAlchemy, which is an ORM (Object Relational Mapper) + SQL toolkit, so that the server doesn't interact with the database directly

--- app/main.py
+++ app/main.py
@@ -1,11 +1,17 @@
-from fastapi import FastAPI, Response, status, HTTPException
+from fastapi import FastAPI, Response, status, HTTPException, Depends
 # from fastapi.params import Body
 from pydantic import BaseModel
-from typing import Optional
-from random import randrange
+# from typing import Optional
+# from random import randrange
 import psycopg2
 from psycopg2.extras import RealDictCursor
 import time
+from sqlalchemy.orm import Session
+from . import models
+from .database import engine, get_db
+
+
+models.Base.metadata.create_all(bind=engine)
 
 
 app = FastAPI()
@@ -16,6 +22,7 @@ class Post(BaseModel):
     title: str
     content: str
     published: bool = True
+    # rating: Optional[int] = None  # Optional field
 
 
 while True:
@@ -54,22 +61,33 @@ def root():  # Path operation function
     return {"message": "Welcome to my API!"}
 
 
+# @app.get("/sqlalchemy")
+# def test_posts(db: Session = Depends(get_db)):
+#     posts = db.query(models.Post).all()
+#     return {"data": posts}
+
+
 # FastAPI goes line by line and runs the first path operation along with the http method that matches the request
 # So the order of the path operations matter
 @app.get("/posts")
-def get_posts():
-    cursor.execute("""SELECT * FROM posts""")
-    posts = cursor.fetchall()
+# def get_posts():
+def get_posts(db: Session = Depends(get_db)):
+    # cursor.execute("""SELECT * FROM posts""")
+    # posts = cursor.fetchall()
+    posts = db.query(models.Post).all()
+
     # print(posts)
     return {"data": posts}
 
 
 @app.get("/posts/{id}")
 # def get_post(id: int, response: Response):
-def get_post(id: int):
+# def get_post(id: int):
+def get_post(id: int, db: Session = Depends(get_db)):
     # post = find_post(id)
-    cursor.execute("""SELECT * FROM posts WHERE id = %s""", (id,))
-    post = cursor.fetchone()
+    # cursor.execute("""SELECT * FROM posts WHERE id = %s""", (id,))
+    # post = cursor.fetchone()
+    post = db.query(models.Post).filter(models.Post.id == id).first()
     if not post:
         # response.status_code = 404
         # response.status_code = status.HTTP_404_NOT_FOUND
@@ -83,37 +101,51 @@ def get_post(id: int):
 @app.post("/posts", status_code=status.HTTP_201_CREATED)
 # Take everything from the request body, convert it to the dictionary and put it in payload
 # def create_posts(payload: dict = Body(...)):
-def create_posts(post: Post):  # Referencing the Post model
+# def create_posts(post: Post):  # Referencing the Post model
+def create_posts(post: Post, db: Session = Depends(get_db)):
     # We expect the payload to have title: str and content: str
     # print(post)  # print(payload)
     # post_dict = post.dict()
     # post_dict['id'] = randrange(0, 1000000)
     # my_posts.append(post_dict)
-    cursor.execute("""INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING *""",
-                   (post.title, post.content, post.published))
-    new_post = cursor.fetchone()
-    conn.commit()
+    # cursor.execute("""INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING *""",
+    #                (post.title, post.content, post.published))
+    # new_post = cursor.fetchone()
+    # conn.commit()
+    new_post = models.Post(
+        # title=post.title, content=post.content, published=post.published
+        **post.dict()  # This does the same thing as above (unpacking the dictionary)
+    )
+    db.add(new_post)
+    db.commit()
+    db.refresh(new_post)
     return {"data": new_post}
 
 
 @app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_post(id: int):
+# def delete_post(id: int):
+def delete_post(id: int, db: Session = Depends(get_db)):
     # index = find_index_post(id)
     # if index == None:
     #     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
     #                         detail=f"Post with {id} was not found")
     # my_posts.pop(index)
-    cursor.execute("""DELETE FROM posts WHERE id = %s RETURNING *""", (id,))
-    deleted_post = cursor.fetchone()
-    if deleted_post == None:
+    # cursor.execute("""DELETE FROM posts WHERE id = %s RETURNING *""", (id,))
+    # deleted_post = cursor.fetchone()
+    deleted_post = db.query(models.Post).filter(models.Post.id == id)
+    # if deleted_post == None:
+    if deleted_post.first() == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"Post with {id} was not found")
-    conn.commit()
+    # conn.commit()
+    deleted_post.delete(synchronize_session=False)
+    db.commit()
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 
 @app.put("/posts/{id}")
-def update_post(id: int, post: Post):
+# def update_post(id: int, post: Post):
+def update_post(id: int, post: Post, db: Session = Depends(get_db)):
     # index = find_index_post(id)
     # if index == None:
     #     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
@@ -121,11 +153,14 @@ def update_post(id: int, post: Post):
     # post_dict = post.dict()
     # post_dict['id'] = id
     # my_posts[index] = post_dict
-    cursor.execute("""UPDATE posts SET title = %s, content = %s, published = %s WHERE id = %s RETURNING *""",
-                   (post.title, post.content, post.published, id))
-    updated_post = cursor.fetchone()
-    if updated_post == None:
+    # cursor.execute("""UPDATE posts SET title = %s, content = %s, published = %s WHERE id = %s RETURNING *""",
+    #                (post.title, post.content, post.published, id))
+    # updated_post = cursor.fetchone()
+    updated_post = db.query(models.Post).filter(models.Post.id == id)
+    if updated_post.first() == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"Post with {id} was not found")
-    conn.commit()
-    return {"data": updated_post}
+    updated_post.update(post.dict(), synchronize_session=False)
+    db.commit()
+    # conn.commit()
+    return {"data": updated_post.first()}
